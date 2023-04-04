3793a2929caeb8d158f47c7a6ca625409e31f0ca
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-04 14:36:21 +0200

    js exercise

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-let listaDellaSpesa = ["Uova", "Latte", "Pane", "Burro"];
+/* PRIMO ESERCIZIO
 
-for (let i = 0; i < listaDellaSpesa.length; i++){
-    console.log((i+1)+ ". " + listaDellaSpesa[i]);
+let listaDellaSpesa = ['Burro' , 'Pane', 'Latte', 'Uova', 'Gelato', 'Acqua'];
 
+for(let i=0; i<listaDellaSpesa.length; i++){
+    console.log((i+1) + ". " + listaDellaSpesa[i])
 }
+
+let contatore = 0;
+
+while(contatore < listaDellaSpesa.length){
+    console.log((contatore+1) + ". " + listaDellaSpesa[contatore]);
+    contatore++;
+}
+
+*/
+
+
